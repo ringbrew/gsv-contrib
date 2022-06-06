c11ea10464c7577abcd587e7e9709ae32eb10b16
ring <ringxu666@gmail.com>
2022-06-06 18:19:08 +0800

    initial project

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ringbrew/gsv-contrib
+
+go 1.16
+
+require (
+	github.com/ringbrew/gsv v0.0.0-20220606060640-124b3aad8e26
+	go.uber.org/zap v1.21.0
+)
---
+++ logger/zaplogger/zap_logger.go
@@ -0,0 +1,132 @@
+package zaplogger
+
+import (
+	"github.com/ringbrew/gsv/logger"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"time"
+)
+
+type Option struct {
+	Output    []string
+	ErrOutput []string
+	TimeKey   string
+	Skip      int
+}
+
+type zapLogger struct {
+	level logger.Level
+	l     *zap.SugaredLogger
+}
+
+func New(opts ...Option) logger.Logger {
+	return newZapLogger(opts...)
+}
+
+func newZapLogger(opts ...Option) *zapLogger {
+	// 自定义时间输出格式
+	customTimeEncoder := func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(t.Format(time.RFC3339Nano))
+	}
+
+	config := zap.NewProductionConfig()
+	config.DisableStacktrace = true
+	config.Encoding = "json"
+	config.OutputPaths = []string{"stdout"}
+	config.ErrorOutputPaths = []string{"stdout"}
+	config.EncoderConfig.TimeKey = "time"
+	config.EncoderConfig.EncodeTime = customTimeEncoder
+	config.EncoderConfig.CallerKey = zapcore.OmitKey
+
+	skip := 0
+
+	if len(opts) > 0 {
+		opt := opts[0]
+		if len(opt.Output) > 0 {
+			config.OutputPaths = opt.Output
+		}
+		if len(opt.ErrOutput) > 0 {
+			config.ErrorOutputPaths = opt.ErrOutput
+		}
+		if opt.TimeKey != "" {
+			config.EncoderConfig.TimeKey = opt.TimeKey
+		}
+
+		if opt.Skip > 0 {
+			skip = opt.Skip
+		}
+	}
+
+	l, _ := config.Build()
+
+	if skip > 0 {
+		l = l.WithOptions(zap.AddCallerSkip(skip))
+	}
+
+	zl := &zapLogger{
+		l: l.Sugar(),
+	}
+
+	return zl
+}
+
+func (l *zapLogger) wrap(entry *logger.LogEntry) *zap.SugaredLogger {
+	wrapL := l.l
+
+	if entry.TraceId != "" {
+		wrapL = wrapL.With(zap.String("trace_id", entry.TraceId))
+	}
+	if entry.SpanId != "" {
+		wrapL = wrapL.With(zap.String("span_id", entry.SpanId))
+	}
+	if entry.ParentId != "" {
+		wrapL = wrapL.With(zap.String("parent_id", entry.ParentId))
+	}
+	if entry.Extra != nil {
+		wrapL = wrapL.With(zap.Any("extra", entry.Extra))
+	}
+
+	return wrapL
+}
+
+func (l *zapLogger) SetLevel(level logger.Level) {
+	l.level = level
+}
+
+func (l *zapLogger) shouldLog(level logger.Level) bool {
+	return l.level <= level
+}
+
+func (l *zapLogger) Debug(entry *logger.LogEntry) {
+	if l.shouldLog(logger.LevelDebug) {
+		l.wrap(entry).Debug(entry.Message)
+	}
+}
+
+func (l *zapLogger) Info(entry *logger.LogEntry) {
+	if l.shouldLog(logger.LevelInfo) {
+		l.wrap(entry).Info(entry.Message)
+	}
+}
+
+func (l *zapLogger) Warn(entry *logger.LogEntry) {
+	if l.shouldLog(logger.LevelWarn) {
+		l.wrap(entry).Warn(entry.Message)
+	}
+}
+
+func (l *zapLogger) Error(entry *logger.LogEntry) {
+	if l.shouldLog(logger.LevelError) {
+		l.wrap(entry).Error(entry.Message)
+	}
+}
+
+func (l *zapLogger) Fatal(entry *logger.LogEntry) {
+	if l.shouldLog(logger.LevelFatal) {
+		l.wrap(entry).Panic(entry.Message)
+	}
+}
+
+func (l *zapLogger) Close() error {
+	return l.l.Sync()
+}
